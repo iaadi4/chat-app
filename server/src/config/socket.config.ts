10f4b2c565b3e709212b37ea416da5775256be73
iaadi4 <adityasingh40675@gmail.com>
2026-01-07 17:06:04 +0530

    feat: add socket.io

---
+++ server/src/config/socket.config.ts
@@ -0,0 +1,39 @@
+import { Server as HttpServer } from "http";
+import { Server, Socket } from "socket.io";
+import { ENV_VARIABLES } from "./env-variables.config";
+import { logger } from "../utils/logger.util";
+
+let io: Server;
+
+export const initializeSocket = (httpServer: HttpServer): Server => {
+  io = new Server(httpServer, {
+    cors: {
+      origin: ENV_VARIABLES.ALLOWED_ORIGINS,
+      credentials: true,
+    },
+    pingTimeout: 60000,
+    pingInterval: 25000,
+  });
+
+  io.on("connection", (socket: Socket) => {
+    logger.info(`Socket connected: ${socket.id}`);
+
+    socket.on("disconnect", (reason) => {
+      logger.info(`Socket disconnected: ${socket.id}, reason: ${reason}`);
+    });
+
+    socket.on("error", (error) => {
+      logger.error(error, `Socket error: ${socket.id}`);
+    });
+  });
+
+  logger.info("Socket.io initialized");
+  return io;
+};
+
+export const getIO = (): Server => {
+  if (!io) {
+    throw new Error("Socket.io not initialized");
+  }
+  return io;
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,16 @@
+import { createServer } from "http";
 import app from "./app";
 import { ENV_VARIABLES } from "./config/env-variables.config";
 import { logger } from "./utils/logger.util";
 import { connectDB } from "./config/db.config";
+import { initializeSocket } from "./config/socket.config";
 
 connectDB();
 
-const server = app.listen(ENV_VARIABLES.PORT, () => {
+const httpServer = createServer(app);
+initializeSocket(httpServer);
+
+const server = httpServer.listen(ENV_VARIABLES.PORT, () => {
   logger.info(`Server running on port ${ENV_VARIABLES.PORT}`);
 });
 
